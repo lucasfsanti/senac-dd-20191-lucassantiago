27135481951ae2b06b4ad9678b3e332660e9bd7f
lucasfsanti <47954905+lucasfsanti@users.noreply.github.com>
2019-03-25 18:52:40 -0300

    Alteração no UsuarioDAO para retornar String

--- senac-dd-20191-lucassantiago/src/exercicio1/model/dao/UsuarioDAO.java
+++ senac-dd-20191-lucassantiago/src/exercicio1/model/dao/UsuarioDAO.java
@@ -10,29 +10,35 @@ import exercicio1.model.vo.UsuarioVO;
 
 public class UsuarioDAO {
 	
-	public int cadastrarUsuarioDAO(UsuarioVO usuarioVO) {
+	public String cadastrarUsuarioDAO(UsuarioVO usuarioVO) {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		int resultado = 0;
+		String mensagem = "";
 		String query = "INSERT INTO USUARIO(NOME, EMAIL, SENHA, IDNIVEL) VALUES ('" + 
 		usuarioVO.getNome() + "', '" + usuarioVO.getEmail() + ", '" + 
 		usuarioVO.getSenha() + "', " + usuarioVO.getNivel().getIdNivel() + ")";
 		try {
 			resultado = stmt.executeUpdate(query);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "Erro ao executar query que cadastra usu·rio!");
 			e.printStackTrace();
 		} finally {
 			Banco.closeStatement(stmt);
 			Banco.closeConnection(conn);
 		}
-		return resultado;
+		if(resultado >=1) {
+			mensagem = "Cadastro efetuado com sucesso!";
+		} else {
+			mensagem = "Erro ao executar query que cadastra usu√°rio!";
+		}
+		return mensagem;
 	}
 	
-	public int alterarUsuarioDAO(UsuarioVO usuarioVO) {
+	public String alterarUsuarioDAO(UsuarioVO usuarioVO) {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		int resultado = 0;
+		String mensagem = "";
 		String query = "UPDATE USUARIO SET NOME = " + usuarioVO.getNome() + 
 				", EMAIL = " + usuarioVO.getEmail() + 
 				", SENHA = " + usuarioVO.getSenha() + 
@@ -41,30 +47,46 @@ public class UsuarioDAO {
 		try {
 			resultado = stmt.executeUpdate(query);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "Erro ao executar a query de alteraÁ„o de usu·rio!");
 			e.printStackTrace();
 		} finally {
 			Banco.closeStatement(stmt);
 			Banco.closeConnection(conn);
 		}
-		return resultado;
+		if(resultado >= 1) {
+			mensagem = "Altera√ß√£o efetuada com sucesso!";	
+		} else {
+			mensagem = "Erro ao executar a query de altera√ß√£o de usu√°rio!";
+		}
+		return mensagem;
 	}
 	
-	public int excluirUsuarioDAO(UsuarioVO usuarioVO) {
+	public String excluirUsuarioDAO(UsuarioVO usuarioVO) {
 		Connection conn = Banco.getConnection();
 		Statement stmt = Banco.getStatement(conn);
 		int resultado = 0;
+		String mensagem = "";
 		String query = "DELETE FROM USUARIO WHERE IDUSUARIO = " + usuarioVO.getId();
 		try {
 			resultado = stmt.executeUpdate(query);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "Erro ao executar a query de exclus„o de usu·rio!");
 			e.printStackTrace();
 		} finally {
 			Banco.closeStatement(stmt);
 			Banco.closeConnection(conn);
 		}
-		return resultado;
+		if(resultado >= 1) {
+			mensagem = "Exclus√£o efetuada com sucesso!";
+		} else {
+			mensagem = "Erro ao executar a query de exclus√£o de usu√°rio!";
+		}
+		return mensagem;
+	}
+	
+	public ArrayList<UsuarioVO> listarUsuarioDAO(UsuarioVO usuarioVO) {
+		Connection conn = Banco.getConnection();
+		Statement stmt = Banco.getStatement(conn);
+		ArrayList<UsuarioVO> = new ArrayList<>;
+		String query = "SELECT USUARIO.NOME, USUARIO.EMAIL, USUARIO.SENHA, NIVEL.DESCRICAO FROM USUARIO LEFT JOIN NIVEL ON USUARIO.IDNIVEL = NIVEL.IDNIVEL"
 	}
 
 
